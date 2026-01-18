bee247284bd5727e46b9f6f811809747015ff406
hyeeun <ha7988@naver.com>
2026-01-18 17:35:10 +0900

    260118

--- phe/boj1655.py
+++ phe/boj1655.py
@@ -1,21 +1,28 @@
-#가운데를 말해요 ( 아직 못품)
-
+#가운데를 말해요
 from heapq import heappush, heappop
 import sys
 
 input = sys.stdin.readline
 
 N = int(input())
-heap = []
+
+left = []  # 최대힙
+right = [] # 최소힙
 
 for _ in range(N):
     num = int(input())
-    heappush(heap, num)
 
-    M = len(heap)
-    print(heap)
-
-    if M % 2 == 0:
-        print(min(heap[M//2], heap[M//2-1]))
+    # 첫번째 값이거나 중간값보다 작은 경우
+    if not left or num <= -left[0]:
+        heappush(left, -num)
     else:
-        print(heap[M//2])
+        heappush(right, num)
+
+    # 왼쪽과 오른쪽 개수 맞춰주기 (항상 왼쪽이 1만큼 더 크거나 같게)
+    if len(left) > len(right) + 1:
+        heappush(right, -heappop(left))
+    elif len(left) < len(right):
+        heappush(left, -heappop(right))
+
+    print(-left[0])
+
